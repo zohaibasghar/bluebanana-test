89313a7859495f3116befa8f35e70912399acb96
zohaib asghar <zohaibmehar123456@gmail.com>
2024-04-20 12:04:12 +0500

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['picsum.photos']
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        pathname: '**',
+      },
+    ],
   }
 };
 
